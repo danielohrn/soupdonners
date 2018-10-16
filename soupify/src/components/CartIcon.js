63db3b8b30d454d57eb7ac031be77cc23abdeaf4
Daniel Öhrn <Daniel.ohrnhasslof@student.kyh.se>
2018-10-16 19:27:17 +0200

    clean up

--- soupify/src/components/CartIcon.js
+++ soupify/src/components/CartIcon.js
@@ -16,6 +16,7 @@ export default class CartIcon extends React.Component {
   }
 
   render() {
+    console.log(this.state, this.props);
     const { shouldSpin } = this.state;
     const { amount } = this.props;
     return (
@@ -53,39 +54,3 @@ export default class CartIcon extends React.Component {
     );
   }
 }
-
-// ({ amount }) => (
-//   <Link
-//     to={"/cart"}
-//     style={{
-//       zIndex: 2,
-//       display: "flex",
-//       justifyContent: "space-around",
-//       alignItems: "center",
-//       position: "fixed",
-//       top: 7,
-//       right: 0,
-//       width: 80,
-//       borderTopLeftRadius: 7,
-//       borderBottomLeftRadius: 7,
-//       background: PRIMARY_GREEN
-//     }}
-//   >
-//     {/* <Link to={"/cart"}> */}
-//     <svg
-//       style={{
-//         // transform: "rotateY(180deg)",
-//         fill: "white",
-//         animation: amount > 0 ? "bouncy 5s infinite linear" : null
-//       }}
-//       xmlns="http://www.w3.org/2000/svg"
-//       width="40"
-//       height="40"
-//       viewBox="0 0 24 24"
-//     >
-//       <path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.304-15l-3.431 12h-2.102l2.542-9h-16.813l4.615 11h13.239l3.474-12h1.929l.743-2h-4.196z" />
-//     </svg>
-//     <span className="is-size-4">{amount}</span>
-//     {/* </Link> */}
-//   </Link>
-// );
